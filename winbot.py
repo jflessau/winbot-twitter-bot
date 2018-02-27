8e1e7e0d416704d6c158ff7bc49d059e28431ff2
jflessau <jan.jf@me.com>
2018-02-27 20:49:45 +0100

    use print as in python 3

--- winbot.py
+++ winbot.py
@@ -26,11 +26,11 @@ twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 def win_things():
     print_settings()
     for i in range(0, tell_settings()["cycles"]):
-        print "\n\n\nstart cycle: " +str(i + 1)
+        print("\n\n\nstart cycle: " +str(i + 1))
         fresh_tweets = search_for_tweets()
         filtered_tweets = filter_tweets(fresh_tweets)
-        print "found " + str(len(fresh_tweets)) + " tweets in this cycle"
-        print str(len(filtered_tweets)) + " tweets left after filtering"
+        print("found " + str(len(fresh_tweets)) + " tweets in this cycle")
+        print(str(len(filtered_tweets)) + " tweets left after filtering")
         print_line(3)
         get_intimate_with(filtered_tweets)
     print_end()
@@ -111,7 +111,7 @@ def tweet_has_string(tweet, str):
 def get_intimate_with(tweet_list):
     limit_counter = len(tweet_list)
     for tweet in tweet_list:
-        print "{"
+        print("{")
         retweet(tweet)
         follow_author(tweet)
         limit_counter = limit_counter + 3
@@ -121,7 +121,7 @@ def get_intimate_with(tweet_list):
             limit_counter = limit_counter + 1
             like_this(tweet)
         limit_counter = wait(limit_counter)
-        print "}\n\n"
+        print("}\n\n")
 
 def find_screen_names_in_text(text, max_users):
     result = []
@@ -148,12 +148,12 @@ def find_screen_names_in_text(text, max_users):
 def retweet(tweet):
     try:
         twitter.retweet(id=tweet["id"])
-        print "retweeted: [tweet_id:" + str(tweet["id"]) + "]"
+        print("retweeted: [tweet_id:" + str(tweet["id"]) + "]")
         date_time = (time.strftime("%d-%m-%Y") + " | " + time.strftime("%H:%M:%S"))
         log_entry = str(date_time) + " | RT | " + str(tweet["id"]) + " | " + tweet["user"]["screen_name"]
         add_line(log_entry, "log.txt")
     except TwythonError as e:
-        print "FAILED to retweet: [tweet_id:" + str(tweet["id"]) + "]"
+        print("FAILED to retweet: [tweet_id:" + str(tweet["id"]) + "]")
 
 def follow_user(screen_name):
     follows = get_list("follows.txt")
@@ -166,9 +166,9 @@ def follow_user(screen_name):
         try:
             twitter.create_friendship(screen_name=screen_name)
             add_line(screen_name, "follows.txt")
-            print "followed " + screen_name.encode("ascii", "ignore")
+            print("followed " + screen_name.encode("ascii", "ignore"))
         except TwythonError as e:
-            print "FAILED to follow " + screen_name.encode("ascii", "ignore")
+            print("FAILED to follow " + screen_name.encode("ascii", "ignore"))
         return (unfollow_add + 1)
     else:
         return unfollow_add
@@ -195,7 +195,7 @@ def follow_mentioned(tweet):
 
 def wait(counter):
     if counter > tell_settings()["interaction_limit"]:
-        print "---sleep for " + str(tell_settings()["sleep"])
+        print("---sleep for " + str(tell_settings()["sleep"]))
         time.sleep(tell_settings()["sleep"])
         return 0
     else:
@@ -211,9 +211,9 @@ def unfollow_fifo():
             add_line(line, "follows.txt")
         try:
             twitter.destroy_friendship(screen_name=last_one)
-            print "unfollowed: " + last_one.encode("ascii", "ignore")
+            print("unfollowed: " + last_one.encode("ascii", "ignore"))
         except:
-            print "FAILED to unfollow " + last_one.encode("ascii", "ignore")
+            print("FAILED to unfollow " + last_one.encode("ascii", "ignore"))
         return 1
     else:
         return 0
@@ -223,18 +223,18 @@ def empty_file(filename):
 
 def print_settings():
     print_line(0)
-    print "--------------------------WinBot----------------------------"
-    print "************************************************************"
-    print "SETTINGS:\nSearch for " + str(tell_settings()["search_for"]) + " tweets in each cycle"
-    print "amount of cycles: " + str(tell_settings()["cycles"])
-    print "sleeps for " + str(tell_settings()["sleep"]) + " seconds"
-    print "whenever bot interacted " + str(tell_settings()["interaction_limit"]) + " times with twitter-api"
-    print "follows max. " + str(tell_settings()["max_mentioned_follow"])  + " users, who were mentioned in a tweet"
-    print "------------------------------------------------------------"
+    print("--------------------------WinBot----------------------------")
+    print("************************************************************")
+    print("SETTINGS:\nSearch for " + str(tell_settings()["search_for"]) + " tweets in each cycle")
+    print("amount of cycles: " + str(tell_settings()["cycles"]))
+    print("sleeps for " + str(tell_settings()["sleep"]) + " seconds")
+    print("whenever bot interacted " + str(tell_settings()["interaction_limit"]) + " times with twitter-api")
+    print "follows max. " + str(tell_settings()["max_mentioned_follow"])  + " users, who were mentioned in a tweet")
+    print("------------------------------------------------------------")
 
 def print_end():
-    print "\n\n\n"
-    print "------------------------------------------------------------\nDONE"
+    print("\n\n\n")
+    print("------------------------------------------------------------\nDONE")
 
 def should_we_like_it(tweet):
     if (tweet_has_string(tweet, "like") or tweet_has_string(tweet, "liking")):
@@ -245,14 +245,14 @@ def should_we_like_it(tweet):
 def like_this(tweet):
     try:
         twitter.create_favorite(id=tweet["id"])
-        print "liked tweet: [tweet_id:" + str(tweet["id"]) + "]"
+        print("liked tweet: [tweet_id:" + str(tweet["id"]) + "]")
     except TwythonError as e:
-        print "FAILED to like tweet: [tweet_id:" + str(tweet["id"]) + "]"
+        print("FAILED to like tweet: [tweet_id:" + str(tweet["id"]) + "]")
 
 def print_line(breaks):
-    print "------------------------------------------------------------"
+    print("------------------------------------------------------------")
     for i in range(0, breaks):
-        print "\n"
+        print("\n")
 
 
 
